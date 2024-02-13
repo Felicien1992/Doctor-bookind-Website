a88056eeca9b5804f5ebe45c321601309427fd1b
Felicien1992 <felicienmukesharugamba@gmail.com>
2024-02-13 12:39:21 +0200

    add hosted link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
  
+export const BASE_URL = 'https://doctor-booking-gq1b.onrender.com'
+
 export const token = localStorage.getItem("token");
